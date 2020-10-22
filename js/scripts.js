9dd765d751e616c101a65ba61ed567750f1182a7
andrei.chebasov <andrei.chebasov@travelline.ru>
2020-10-22 23:39:46 +0300

    оживление страницы

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,81 @@
 const loginLink = document.querySelector(".js-modal");
 const loginPopup = document.querySelector(".popup");
 const loginClose = loginPopup.querySelector(".modal-close");
-const btn_slide1 = document.querySelector(".button1");
-const btn_slide2 = document.querySelector(".button2");
-const btn_slide3 = document.querySelector(".button3");
-const slide1 = document.querySelector(".banner-slide-1");
-const slide2 = document.querySelector(".banner-slide-2");
-const slide3 = document.querySelector(".banner-slide-3");
+const popupForm = document.querySelector(".appointment-form");
+
+popupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if(popupForm.name.value==="" || popupForm.email.value===""){
+    loginPopup.classList.remove("modal-error");
+    loginPopup.classList.add("modal-error");
+  }
+});
 
 loginLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.add("modal-show");
-
-  if (storage) {
-    loginLogin.value = storage;
-    loginPassword.focus();
-  } else {
-    loginLogin.focus();
-  }
+  document.body.style.overflow="hidden";
 });
 
 loginClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   loginPopup.classList.remove("modal-show");
   loginPopup.classList.remove("modal-error");
+  document.body.style.overflow="unset";
 });
 
-btn_slide1.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  btn_slide1.classList.add("remove");
-  btn_slide2.classList.remove("current");
-  btn_slide3.classList.remove("current");
-  slide1.classList.add("slide-current");
-  slide2.classList.remove("slide-current");
-  slide3.classList.remove("slide-current");
+const sliderControls = document.querySelectorAll(".slider-controls button"),
+  sliderElement=document.querySelectorAll(".banner-slide");
+
+function clearClass(element, className) {
+  element.forEach(function (item) {
+    item.classList.remove(className);
+  });
+}
+
+sliderControls.forEach(function (button, indexControl) {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    //Очистка
+    clearClass(sliderControls, 'current');
+    clearClass(sliderElement, 'slide-current');
+    sliderElement.forEach(function (slide, slideIndex) {
+      if(indexControl===slideIndex){
+        slide.classList.add("slide-current");
+      }
+    });
+    //добавление
+    button.classList.add("current");
+  });
 });
 
-btn_slide2.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  btn_slide1.classList.remove("remove");
-  btn_slide2.classList.add("current");
-  btn_slide3.classList.remove("current");
-  slide1.classList.remove("slide-current");
-  slide2.classList.add("slide-current");
-  slide3.classList.remove("slide-current");
-});
+//карта
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+      center: [59.938635, 30.323118],
+      zoom: 9
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
 
-btn_slide3.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  btn_slide1.classList.remove("remove");
-  btn_slide2.classList.remove("current");
-  btn_slide3.classList.add("current");
-  slide1.classList.remove("slide-current");
-  slide2.classList.remove("slide-current");
-  slide3.classList.add("slide-current");
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Собственный значок метки',
+      balloonContent: 'Это красивая метка'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/map-marker.png',
+      // Размеры метки.
+      iconImageSize: [231, 190],
+    });
+
+  myMap.geoObjects
+    .add(myPlacemark);
 });
